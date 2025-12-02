6c5e911172ccb5f67ab06b85767eb1b8d3644a93
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-12-02 09:59:43 -0600

    python basics

---
+++ hello.py
@@ -0,0 +1,39 @@
+# This program is practice to ask for hello, and ask for your name.
+
+print("-----------------------------")
+
+print("Hello, User!")
+
+print("-----------------------------")
+
+print("What is your name?")
+userName = input()
+
+print("-----------------------------")
+
+print("It is good to meet you, " + userName)
+
+print("-----------------------------")
+
+print("The legnth of your name is: " + str(len(userName)))
+
+print("-----------------------------")
+
+print("What is your age")
+userAge = input()
+
+print("-----------------------------")
+
+print("You will be " + str(int(userAge) + 1) + " in a year.")
+
+print("-----------------------------")
+
+# Practice Questions
+print("------Practice Data Types--------")
+print('str(0): '+ str(0))
+print('str(-3.14): '+ str(-3.14))
+print("int('42'): " + str(int('42')))
+print('int(1.25): '+ str(int(1.25)))
+print('int(1.99): '+ str(int(1.99)))
+print("float(/'3.14/'): "+ str(float('3.14')))
+print('float(10): '+ str(float(10)))
